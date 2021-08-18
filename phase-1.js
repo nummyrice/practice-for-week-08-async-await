c8399e00530aaa1cfd64300ff52a57104cb833eb
nummyrice <nicholasroyrice@gmail.com>
2021-08-18 10:47:43 -0700

    somehow it worked

--- phase-1.js
+++ phase-1.js
@@ -27,12 +27,14 @@ function liftWeights() {
 
 // refactor this function to handle Promises using async/await instead of
   // .then and .catch
-function workout() {
-  stretch()
-    .then(runOnTreadmill)
-    .then(liftWeights)
-    .then(() => console.log("done working out"))
-    .catch((err) => console.log(err));
+async function workout() {
+  try {
+    await stretch();
+    await runOnTreadmill();
+    await liftWeights();
+    await (() => {console.log("done working out")})();
+  }
+    catch {(err) => console.log(err)};
 }
 
 /* ============================ TEST YOUR CODE ============================
